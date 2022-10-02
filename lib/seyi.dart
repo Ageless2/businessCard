216ef46a12d71c180c65d0f21a1108e93b559926
Ageless2 <damilolalawrence235@gmail.com>
2022-10-02 17:39:34 +0100

    added a new Class

---
+++ lib/seyi.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Chess extends StatelessWidget {
+  const Chess({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
